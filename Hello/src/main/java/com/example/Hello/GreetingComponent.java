8d6432f0aebdbad22095ccd95f99779743cba302
SLAKE\PCJZONG <jiazhen.ong@silverlakeaxis.com>
2022-07-05 14:48:15 +0800

    first commit

---
+++ Hello/src/main/java/com/example/Hello/GreetingComponent.java
@@ -0,0 +1,11 @@
+package com.example.Hello;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GreetingComponent {
+
+	public String getMessage() {
+		return "Hello from GreetingComponent";
+	}
+}
